69297447b33d91b80c1be1b29c6afbba311b7d7c
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-11 12:35:44 -0400

    Screen: edit profile screen

---
+++ src/screens/EditProfileScreen.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EditProfileScreen = () => {
+  const [pronouns, setPronouns] = useState('');
+  const [bio, setBio] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+     headerBackTitle: "Back"
+    })
+  }, [navigation])
+
+  const submit = () => {}
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+    <StatusBar style="light" />
+
+    <Text h3 style={{marginBottom: 50}}>
+      Edit Profile
+    </Text>
+    <View style={styles.inputContainer}>
+      <Input
+        placeholder="Pronouns"
+        autofocus
+        type="text"
+        value={pronouns}
+        onChangeText={(text) => setPronouns(text)}
+      />
+
+      <Input
+        placeholder="Bio"
+        type="text"
+        value={bio}
+        onChangeText={(text) => setBio(text)}
+      />
+
+      <Input
+        placeholder="Profile Picture Url"
+        type="text"
+        value={imageUrl}
+        onChangeText={(text) => setImageUrl(text)}
+      />
+
+    </View>
+    <Button buttonStyle={styles.button} onPress={submit} title ="Submit" />
+    <View style={{height: 100}} />
+  </KeyboardAvoidingView>
+  )
+}
+
+export default EditProfileScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    borderColor: '#E8984E',
+    borderWidth: 5,
+    width: 200,
+    margin: 5,
+  }
+})
