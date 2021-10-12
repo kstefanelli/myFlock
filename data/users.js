328949ea804c68d80a589ba8489e327f63717b2e
Tiya Lee <Bigtiyalee@gmail.com>
2021-10-11 21:22:57 -0400

    add stories in addInterest page

--- data/users.js
+++ data/users.js
@@ -7,7 +7,7 @@ export const USERS = [
         pronouns: "she/her/hers",
         profileImage: "http://media3.s-nbcnews.com/i/MSNBC/Components/Photo/_new/tdy-120822-joyce-carpati-02.jpg",
         intro: "I was in a news article about sharing my best beauty tips",
-        interests: ['crafting', 'chickens', 'yoga', 'baking', 'reading', 'writing'],
+        interests: ['crafting', 'chickens', 'yoga', 'Cooking or baking', 'reading', 'writing', 'Travel', "Graphic design"],
     },
     {
         userId: 124,
@@ -17,7 +17,7 @@ export const USERS = [
         pronouns: "she/her/hers",
         profileImage: "https://media.self.com/photos/57d8de3946d0cb351c8c6f92/master/w_354,h_536,c_limit/Screen%20shot%202011-09-19%20at%206.35.28%20PM.png",
         intro: "It wasn't until I was 75 that I seriously considered becoming a fitness instructor.",
-        interests: ['crafting', 'jogging', 'yoga', 'movies', 'reading'],
+        interests: ['crafting', 'jogging', 'yoga', 'movies', 'reading', "Graphic design"],
     },
     {
         userId: 125,
@@ -27,7 +27,7 @@ export const USERS = [
         pronouns: "she/her/hers",
         profileImage: "https://www.fieldandstream.com/uploads/2019/01/18/NDZTQH2MAT7CAKUQDZDAP5JTVI.png",
         intro: "I fought off a rabid animal by drawing on my martial arts training. Not even kidding.",
-        interests: ['karate', 'baking', 'bird watching', 'hiking', 'reading'],
+        interests: ['karate', 'Cooking or baking', 'bird watching', 'hiking','Tennis', 'reading', 'Skiing','Community service'],
     },
     {
         useId: 126,
@@ -37,7 +37,7 @@ export const USERS = [
         pronouns: "she/her/hers",
         profileImage: "https://wboc.images.worldnow.com/images/8870055_BG1.jpg",
         intro: "Once, when there was an intruder in my house, I made him call the police on himself.",
-        interests: ['true crime podcasts', 'fine dining', 'art', 'baking', 'reading'],
+        interests: ['true crime podcasts', 'fine dining', 'art', 'Cooking or baking', 'reading'],
 
     },
     {
@@ -48,6 +48,86 @@ export const USERS = [
         pronouns: "she/her/hers",
         profileImage: "https://i.dailymail.co.uk/i/pix/2012/03/10/article-2112984-121A8D01000005DC-526_468x461.jpg",
         intro: "I rate restaurants. I know the best places to eat in town.",
-        interests: ['crafting', 'poetry', 'yoga', 'baking', 'fine dining', 'reading', 'writing', 'dancing'],
-    }
+        interests: ['crafting', 'poetry', 'yoga', 'Cooking or baking', 'fine dining', 'reading', 'writing', 'dancing'],
+    },
+    {
+        id: 1,
+        username: 'Ms. Irving Terry',
+        intro: "Charlotte D'Amore",
+        email: 'Viva_Koch32@gmail.com',
+        profileImage: 'http://placeimg.com/640/489',
+        interests: ["Travel"]
+      },
+      {
+        id: 2,
+        username: 'Miss Reginald Rippin',
+        intro: 'Rosemary Barrows',
+        email: 'Anastacio.Kutch@yahoo.com',
+        profileImage: 'http://placeimg.com/640/481',
+        interests: ["Travel"]
+      },
+      {
+        id: 3,
+        username: 'Victor Prohaska',
+        intro: 'Alberta Dickinson',
+        email: 'Damian_Kuhn96@gmail.com',
+        profileImage: 'http://placeimg.com/640/482',
+        interests: ["Travel","Skiing"]
+      },
+      {
+        id: 4,
+        username: 'Roosevelt Fritsch',
+        intro: 'Jamie Goodwin',
+        email: 'Brett_Weissnat@hotmail.com',
+        profileImage: 'http://placeimg.com/640/483',
+        interests: ["Skiing"]
+      },
+      {
+        id: 5,
+        username: 'Lora Marks',
+        intro: "Austin O'Keefe",
+        email: 'Teagan_Schmidt@hotmail.com',
+        profileImage: 'http://placeimg.com/640/485',
+        interests: ["Skiing"]
+      },
+      {
+        id: 6,
+        username: 'Jean Erdman',
+        intro: 'Inez Nolan',
+        email: 'Soledad.Bogisich@yahoo.com',
+        profileImage: 'http://placeimg.com/640/484',
+        interests: ["Skiing"]
+      },
+      {
+        id: 7,
+        username: 'Irma Frami',
+        intro: 'Orville Renner',
+        email: 'Doug.Weber49@gmail.com',
+        profileImage: 'http://placeimg.com/640/486',
+        interests: ["Skiing"]
+      },
+      {
+        id: 8,
+        username: 'Marcus Konopelski PhD',
+        intro: 'Eileen Bayer',
+        email: 'Michel.Boyle@yahoo.com',
+        profileImage: 'http://placeimg.com/640/487',
+        interests:["Skiing"]
+      },
+      {
+        id: 9,
+        username: 'Maryann Gorczany',
+        intro: 'Joseph Batz',
+        email: 'Chyna.Mueller35@hotmail.com',
+        profileImage: 'http://placeimg.com/640/488',
+        interests: ["Skiing"]
+      },
+      {
+        id: 10,
+        username: 'Sheryl Murazik',
+        intro: 'Jessica Senger',
+        email: 'Dereck6@gmail.com',
+        profileImage: 'http://placeimg.com/640/489',
+        interests: ["Community service"]
+      }
 ]
---
+++ src/components/Stories.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { View, Text, Image ,StyleSheet} from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import {USERS} from '../../data/users'
+
+
+
+const Stories = ({interest}) => {
+    const filteredUsers = USERS.filter(user=>user.interests.indexOf(interest)>-1)
+    return (
+        <View >
+            <ScrollView horizontal 
+            showsHorizontalScrollIndicator={false}
+            >
+                {
+             
+                    filteredUsers.map((story,index)=>(
+                        <View key = {index} style={{alignItems:'center'}}>
+                        <Image 
+                        source = {{uri:story.profileImage}} 
+                        style = {styles.story}
+                        />
+                        <Text style={{color:'white'}}>{
+                        story.username.length>9
+                        ? story.username.slice(0,9)+'...'
+                        : story.username
+                        }</Text>
+                        </View>
+                    ))
+                }
+                </ScrollView>
+            <Text ></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+story:{
+    width:70,
+    height:70,
+    borderRadius:50,
+    marginLeft:6,
+    borderWidth:2,
+    borderColor:'#ff8501'
+
+}
+})
+
+export default Stories
