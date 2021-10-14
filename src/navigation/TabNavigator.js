e503acd3ed18dfe05ecac6d083a38b1be4636269
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-14 13:01:15 -0400

    This is a combination of 4 commits.
    cleanup
    
    feat: added RegisterScreen
    
    feat: added RegisterScreen
    
    added button
    
    updated firestore.js with db and auth
    
    updated styling
    
    updated .gitignore
    
    updated
    
    Delete .expo/web/cache directory
    
    deleting extra files
    
    Feat: chat

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StackNavigator from './StackNavigator';
+import AddInterest from '../screens/AddInterest';
+import LoginScreen from '../screens/Login';
+import EditProfileScreen from '../screens/EditProfileScreen';
+import ProfileView from '../screens/ProfileView';
+import NestViewScreen from '../screens/NestViewScreen'
 
 //whatever you do, MAKE SURE YOU ADD A PERIOD IN BETWEEN TAB.NAVIGATOR ELSE IT WILL NOT COMPILE
 //YOU WILL SPEND HOURS WITH A BLACK SCREEN AND NO CONSOLE.LOG NOT KNOWING WHAT HAPPENED
@@ -11,6 +17,10 @@ function TabNavigator(props) {
 	return (
 		<Tab.Navigator>
 			<Tab.Screen name="StackNavigator" component={StackNavigator} />
+			<Tab.Screen name="Add Interest" component={AddInterest} />
+			<Tab.Screen name="Login" component={LoginScreen} />
+			<Tab.Screen name="Nest View" component={NestViewScreen} />
+			<Tab.Screen name="Profile View" component={ProfileView} />
 		</Tab.Navigator>
 	);
 }
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
+// import { auth } from '../firebase';
+
+const RegisterScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [pronouns, setPronouns] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const register = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        authUser.user.update({
+          displayName: name,
+          photoURL:
+            imageUrl || 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/1200',
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create an account
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Name"
+          autofocus
+          type="text"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          placeholder="Pronouns"
+          type="text"
+          value={pronouns}
+          onChangeText={(text) => setPronouns(text)}
+        />
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Profile Picture Url (optional)"
+          type="text"
+          value={imageUrl}
+          onChangeText={(text) => setImageUrl(text)}
+          onSubmitEditing={register}
+        />
+      </View>
+      <Button buttonStyle={styles.button} onPress={register} title="Register" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+
+
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#E6E8DA',
+  },
+  button: {
+    backgroundColor: '#1F142E',
+    borderColor: '#1F142E',
+    borderWidth: 5,
+    width: 200,
+    margin: 5,
+  },
+  inputContainer: {
+    width: 300,
+  },
+});
