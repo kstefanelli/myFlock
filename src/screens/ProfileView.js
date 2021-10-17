36a1936351a036c41b0b78460ab28e013e57c72a
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-10-16 21:50:04 -0400

    profile edits not pulled previously

--- src/screens/ProfileView.js
+++ src/screens/ProfileView.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import FriendsList from '../sub-components/FriendsList';
 import { USERS } from '../../data/users';
 
@@ -11,8 +11,17 @@ const ProfileView = () => {
 			<Text style={styles.profileName}>Meet {user.username}!</Text>
 			<Image source={{ uri: user.profileImage }} style={styles.profileImage} />
 			<Text>Bio: {user.intro} </Text>
+			<Text>Pronouns: {user.pronouns}</Text>
+			<ScrollView horizontal showsHorizontalScrollIndicator={false}><Text>Interests: </Text>
+                {user.interests.map((interest, index) => (
+                    <View key={index} style={{alignItems: 'center'}}>
+                        <Text style={{color: '#1f142e'}}>{interest.toLowerCase()}, </Text>
+                    </View>
+                ))}
+            </ScrollView>
 			<Text>Interests: {user.interests}</Text>
 			<FriendsList />
+			<Text>Location: {user.location.name}</Text>
 		</View>
 	);
 };
@@ -27,8 +36,8 @@ const styles = StyleSheet.create({
 		width: 250,
 		marginBottom: 20,
 		borderRadius: 125,
-		borderWidth: 10,
-		borderColor: '#bf90b1',
+		borderWidth: 8,
+		borderColor: '#e8984e',
 		alignItems: 'center',
 	},
 	profileName: {
