b8310c56e9f29984f598c2b564f5149efa769a6e
Tiya Lee <Bigtiyalee@gmail.com>
2021-10-20 14:58:11 -0400

    fixed 3 small  bugs

--- src/screens/ProfileView.js
+++ src/screens/ProfileView.js
@@ -10,64 +10,67 @@ import { useFocusEffect } from '@react-navigation/native';
 
 
 const ProfileView = () => {
-	// //this user alternative is to pull from dummy data to display a profile
-	// //   const user = USERS[0];
-  // //line 14 might not be enough. It used to work, but now, when we open the app, it initially doesn't (async authorization?). If you comment out all calls to user and then comment it back in it suddenly works. Except there must be another layert of access to get to user features not included in auth. pronouns, bio, interests (again, used to work :( ))
-	// const [userData, setUserData] = useState([])
+  if(!auth){
+  return <Text>Please Login or Sign Up!</Text>
+  }else{
+	//this user alternative is to pull from dummy data to display a profile
+	//   const user = USERS[0];
+  //line 14 might not be enough. It used to work, but now, when we open the app, it initially doesn't (async authorization?). If you comment out all calls to user and then comment it back in it suddenly works. Except there must be another layert of access to get to user features not included in auth. pronouns, bio, interests (again, used to work :( ))
+	const [userData, setUserData] = useState([])
 
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //   console.log('usefocus triggered2');
-  //   const unsubscribe = () => {
-  //     db.collection('Users')
-  //     .where('email', ''=='', auth.currentUser.email)
-  //       .onSnapshot((snapshot)=> {
-  //         setUserData(snapshot.docs.map(doc=> ({
-  //           id: doc.id,
-  //           data: doc.data(),
-  //         })))
-  //       });
-  //     }
-  //   return unsubscribe();
-  //  }, [])
-  //  );
+  useFocusEffect(
+    React.useCallback(() => {
+    console.log('usefocus triggered2');
+    const unsubscribe = () => {
+      db.collection('Users')
+      .where('email', ''=='', auth.currentUser.email)
+        .onSnapshot((snapshot)=> {
+          setUserData(snapshot.docs.map(doc=> ({
+            id: doc.id,
+            data: doc.data(),
+          })))
+        });
+      }
+    return unsubscribe();
+   }, [])
+   );
 
 
-  // const logOutUser = () => {
-  //   auth.signOut().then(()=>{
-  //     navigation.navigate('Login')
-  //   })
-  // };
+  const logOutUser = () => {
+    auth.signOut().then(()=>{
+      navigation.navigate('Login')
+    })
+  };
 
-  // return (
-  //   <View style={styles.profileView}>
-	// 	<Text style={styles.profileName}>Hello, {userData.displayName}! </Text>
+  return (
+    <View style={styles.profileView}>
+		<Text style={styles.profileName}>Hello, {userData.displayName}! </Text>
 
-	// 	<>
-  //       <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>({userData.pronouns})</Text>
-	// 	</>
-  //     <Image source={{ uri: userData.photoURL }} style={styles.profileImage} />
-  //     <>
-  //       <Text style={{ fontWeight: 'bold' }}>About You:</Text>
-  //       <Text>{userData.bio} </Text>
-  //     </>
-	// {/*the majority of our database users do not have location information, so if I call name from this array, it does not work. For now, I don't think there's a workaround for displaying location name*/}
-  //   {/* <Text style={{ fontWeight: 'bold' }}>Your Location: </Text> */}
-  //   {/* <Text>{currentUser.location.name} </Text> */}
+		<>
+        <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>({userData.pronouns})</Text>
+		</>
+      <Image source={{ uri: userData.photoURL }} style={styles.profileImage} />
+      <>
+        <Text style={{ fontWeight: 'bold' }}>About You:</Text>
+        <Text>{userData.bio} </Text>
+      </>
+	{/*the majority of our database users do not have location information, so if I call name from this array, it does not work. For now, I don't think there's a workaround for displaying location name*/}
+    {/* <Text style={{ fontWeight: 'bold' }}>Your Location: </Text> */}
+    {/* <Text>{currentUser.location.name} </Text> */}
 
-  //     <Text style={{ fontWeight: 'bold' }}>Your Interests: </Text>
-  //     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-  //       {/* {userData.interests.map((interest, index) => (
-  //         <View key={index} style={{ alignItems: 'center' }}>
-  //           <Text style={{ color: '#1f142e' }}>{interest}, </Text>
-  //         </View>
-  //       ))} */}
-  //     </ScrollView>
-  //     <Button buttonStyle={styles.button} title="Log Out" onPress={logOutUser} />
-  //   </View>
-  // );
+      <Text style={{ fontWeight: 'bold' }}>Your Interests: </Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {/* {userData.interests.map((interest, index) => (
+          <View key={index} style={{ alignItems: 'center' }}>
+            <Text style={{ color: '#1f142e' }}>{interest}, </Text>
+          </View>
+        ))} */}
+      </ScrollView>
+      <Button buttonStyle={styles.button} title="Log Out" onPress={logOutUser} />
+    </View>
+  );
+      }
 
-  return (<Text>profile view</Text>)
 };
 
 const styles = StyleSheet.create({
