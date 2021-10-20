d8fd0289e58d0c6eb581938b84c2b3155f25640e
Suzanne Wong <suzannewongfl@gmail.com>
2021-10-20 16:02:53 -0400

    LogIn redirects to TabNavigator

--- App.js
+++ App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import DrawerNavigator from './src/navigation/DrawerNavigator';
-import Login from './src/screens/Login';
-import RegisterScreen from './src/screens/RegisterScreen';
-import ChatScreen from './src/screens/ChatScreen';
-import AddInterest from './src/screens/AddInterest';
 import TabNavigator from './src/navigation/TabNavigator';
-import Map from './src/screens/Map';
+import ProfileViewNavigator from './src/screens/ProfileViewNavigator';
 
 const Stack = createStackNavigator();
 
 function App() {
-	const isLoggedIn = true;
+	const isLoggedIn = false;
 
 	return (
 		<>
@@ -25,14 +19,7 @@ function App() {
 						<TabNavigator />
 					</>
 				) : (
-					<Stack.Navigator headerShown="false">
-						<>
-							<Stack.Screen name="LogIn" component={Login} />
-							<Stack.Screen name="Register" component={RegisterScreen} />
-							{/* <Stack.Screen name="Drawer" component={DrawerNavigator} /> */}
-							<Stack.Screen name="Map" component={Map} />
-						</>
-					</Stack.Navigator>
+					<ProfileViewNavigator />
 				)}
 			</NavigationContainer>
 		</>
