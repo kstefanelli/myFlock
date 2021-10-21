c203fd3b6ab34da7ff4a647f8a8ccbb1b24be126
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-21 10:32:06 -0400

    style: connected new styled version to FB and restricted view to only chats user is party to

--- src/components/EggItem.js
+++ src/components/EggItem.js
@@ -1,23 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {ListItem, Avatar} from 'react-native-elements';
+import {auth} from '../../firebase';
+import { useFocusEffect } from '@react-navigation/native';
 
 
-const EggItem = ({id, chatName, enterChat}) => {
+const EggItem = ({id, photos, enterChat}) => {
+  const [eggPicture, setEggPicture] = useState("")
+
+  useFocusEffect(
+    React.useCallback(() => {
+    const unsubscribe = () => {
+      if(photos !== undefined) {
+        for (let i =0; i<photos.length; i++){
+          let pic = photos[i];
+          if (pic !==undefined && pic !== auth.currentUser.photoURL){setEggPicture(pic); break}
+          else {
+            setEggPicture("https://literaryyard.files.wordpress.com/2017/11/smiling-face-funny-bird-picture.jpg?w=639)"
+          )
+        }
+      }
+      }
+      else {
+        setEggPicture("https://literaryyard.files.wordpress.com/2017/11/smiling-face-funny-bird-picture.jpg?w=639)"
+          )
+      }
+      };
+
+    return unsubscribe();
+   }, [])
+   );
+
   return (
     <View >
-      <ListItem onPress={()=>enterChat(id, chatName)}
+      <ListItem
+      containerStyle={{backgroundColor: "#E6E8DA"}}
+      onPress={()=>enterChat(id)}
       key ={id}
+      topDivider
       bottomDivider>
         <Avatar
           rounded
           source={{
-            uri: 'https://image.freepik.com/free-vector/cute-avocado-cat-cartoon-character-animal-fruit-isolated_138676-3184.jpg',
+            uri: eggPicture
           }}
         />
-        <ListItem.Content>
-          <ListItem.Title style={{fontWeight: '800', color: '#354A18'}}>{chatName}</ListItem.Title>
+        <ListItem.Content >
+          <ListItem.Title style={{fontWeight: '800', color: '#354A18'}}>{id}</ListItem.Title>
           <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail"
           style={{color: '#354A18'}}>
             This is a test subtitle. I love cats. Dogs are your best friend but cats don't tell
@@ -32,5 +62,8 @@ const EggItem = ({id, chatName, enterChat}) => {
 export default EggItem;
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E6E8DA'
+  }
 });
 
--- src/screens/NestViewScreen.js
+++ src/screens/NestViewScreen.js
@@ -1,79 +1,118 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, {useLayoutEffect, useState, useEffect} from 'react'
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native'
-import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
-import {AntDesign, SimpleLineIcons } from "@expo/vector-icons"
+import React, {useState, useLayoutEffect, useEffect} from 'react'
+import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native'
+import {Text, Avatar} from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import EggItem from '../components/EggItem'
+import {FontAwesome5} from '@expo/vector-icons';
+import { useFocusEffect } from '@react-navigation/native';
 import { auth, db } from '../../firebase';
 
 
 const NestViewScreen = ({navigation}) => {
   const [chats, setChats] = useState([])
 
+  useFocusEffect(
+    React.useCallback(() => {
+    console.log('usefocus triggered');
+    const unsubscribe = () => {
+      db.collection("chats").
+      where('parties', 'array-contains', auth.currentUser.email)
+      .onSnapshot((snapshot)=> {
+        setChats(snapshot.docs.map(doc=> ({
+          id: doc.id,
+          data: doc.data(),
 
-  useEffect( ()=> {
-    const unsubscribe = db.collection("chats").onSnapshot((snapshot)=> {
-      setChats(snapshot.docs.map(doc=> ({
-        id: doc.id,
-        data: doc.data(),
-      })))
-    });
-    return unsubscribe;
+        })))
+      });
+      {noChat()}
+    }
+    return unsubscribe();
   }, [])
+  );
+
+  const enterChat = (id) => {
+    navigation.navigate("ChatScreen",{chatName:id})
+  };
+
+  const noChat = () => {
+
+
+    if (chats.length < 1) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <FontAwesome5 name="earlybirds" size={200} color="black" />
+
+          <TouchableOpacity
+            style={{alignItems: 'center', justifyContent: 'center', marginTop: 50}}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={{fontSize: 18, color: 'black', textAlign: 'center'}}>
+              No chirps yet! Fly over the map to peek at other birds with your interests!
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.eggContainer}>
+          {chats.map(({id, data:{photos}}) => (
+            <EggItem key ={id}
+            id={id}
+            photos={photos}
+            enterChat={enterChat} />
+          ))}
+        </View>
+      );
+    }
+  };
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: "Meow Talk",
-      headerStyle: {
-        backgroundColor: "#FFF",
-      },
-      headerTitleStyle: {
-        color: "#354A18",
-      },
-      headerTintColor: "white",
-      headerLeft: () => (
-      <View style ={{marginLeft: 20}} >
-        <TouchableOpacity onPress={alert('hello!')}
-        activeOpacity={0.5}>
-        <Avatar rounded source={{uri: auth?.currentUser?.photoURL}} />
-        </TouchableOpacity>
-      </View>),
-      headerRight: () => (
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: 80,
-          marginRight:20,
-        }} >
-        <TouchableOpacity activeOpacity={0.5}>
-          <AntDesign name="camerao" size={24} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>navigation.navigate('AddChat')}
-        activeOpacity={0.5}>
-          <SimpleLineIcons name="pencil" size={24} color="black" />
-        </TouchableOpacity>
-        </View>
-    ),
-    });
+      title: "Nest View"
+    })
   }, [navigation])
 
-const enterChat = (id, chatName) => {
-navigation.navigate("Chat", {
-  id, chatName
-})
-}
-
   return (
-    <SafeAreaView >
+    <SafeAreaView style={{backgroundColor: "#E6E8DA"}} >
       <ScrollView style={styles.container}>
-        {chats.map(({id, data: {chatName}})=> (
+      <Text h2 style={{marginTop: 10, marginBottom: 10, textAlign: 'center'}}>
+          Birds of a Feather
+        </Text>
+
+        <View
+          style={{
+            marginBottom: 10,
+            borderBottomColor: '#e8984e',
+            borderBottomWidth: 5,
+          }}
+        />
+        {/* {chats.map(({id, data: {chatName}})=> (
           <EggItem key ={id}
           id={id}
           chatName={chatName}
           enterChat={enterChat} />
-        ))}
+        ))} */}
+        {noChat()}
+
+        <View
+          style={{
+            marginTop: 10,
+            borderBottomColor: '#e8984e',
+            borderBottomWidth: 5,
+          }}
+        />
+
+        <Text h2 style={{marginTop: 10, marginBottom: 10, textAlign: 'center'}}>
+          Flock Together
+        </Text>
       </ScrollView>
     </SafeAreaView>
   )
