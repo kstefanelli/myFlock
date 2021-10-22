65e228dfc02ff4a8fd89b9c80db88ab7e5016f13
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-22 15:01:05 -0400

    Patch: location on user instance changed to string from array

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -30,8 +30,9 @@ const register = () => {
         imageUrl: imageUrl || 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/1200',
         age: 0,
         bio: "",
+        pronouns: pronouns,
         interests: [],
-        location: []
+        location: "",
       })
       )
       .catch((error) => alert(error.message));
