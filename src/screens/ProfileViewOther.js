49b7e82503abe459e144e90630c79eeae7ca867a
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-26 17:28:33 -0400

    Bug: fixed email query to firebase where google auth profiles have lowercased emails v sentence case in firestore

--- src/screens/ProfileViewOther.js
+++ src/screens/ProfileViewOther.js
@@ -1,37 +1,45 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { Button } from 'react-native-elements';
-//{USERS} is only for if you need to populate with dummy data once hooked up, delete this line
-import { USERS } from '../../data/users';
-import { auth, db } from '../../firebase';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import {Button} from 'react-native-elements';
+
+import {auth, db} from '../../firebase';
 import ProfileViewNavigator from '../navigation/ProfileNavigator';
+import * as firebase from 'firebase';
 
-const ProfileViewOther = ({ navigation, route }) => {
-	viewedUser = route.params.user[route.params.idx].data;
+const ProfileViewOther = ({navigation, route}) => {
+  const viewedUser = route.params.user[route.params.idx].data;
+  const thisChatName = Math.floor(Math.random() * 10000).toString();
 
-	const startNewChat = () => {
-		db.collection('chats').doc(thisChatName).set(
-			{
-				chatName: thisChatName,
-			},
-			{ merge: true }
-		);
-		db.collection('chats')
-			.doc(thisChatName)
-			.update({
-				users: db.FieldValue.arrayUnion(auth.currentUser.uid, viewedUser.uid),
-			})
-			.then(() => {
-				navigation.navigate('Chat', {
-					user: viewedUser,
-				});
-			});
-	};
-	return (
-		<View style={styles.profileView}>
-			<Text style={styles.profileName}>Meet {viewedUser.name}!</Text>
+	const currentEmail =
+	auth.currentUser.email.charAt(0).toUpperCase() + auth.currentUser.email.slice(1);
+
+  console.log('user>', viewedUser);
+
+  const startNewChat = () => {
+    db.collection('chats').doc(thisChatName).set(
+      {
+        chatName: thisChatName,
+      },
+      {merge: true}
+    );
+    db.collection('chats')
+      .doc(thisChatName)
+      .update({
+        parties: firebase.firestore.FieldValue.arrayUnion(currentEmail, viewedUser.email),
+        photos: firebase.firestore.FieldValue.arrayUnion(
+          auth.currentUser.photoURL,
+          viewedUser.imageUrl
+        ),
+      });
+
+    navigation.navigate('ChatScreen', {chatName: thisChatName});
+  };
+  return (
+    <View style={styles.profileView}>
+      {/* <Text>hi</Text> */}
+      <Text style={styles.profileName}>Meet {viewedUser.name}!</Text>
 
 			<Image source={{ uri: viewedUser.imageUrl }} style={styles.profileImage} />
 			<>
@@ -55,66 +63,66 @@ const ProfileViewOther = ({ navigation, route }) => {
 				))}
 			</ScrollView>
 			<Button buttonStyle={styles.button} onPress={startNewChat} title="Start Chatting" />
-		</View>
-	);
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	button: {
-		backgroundColor: '#1F142E',
-		borderColor: '#bf90b1',
-		borderWidth: 5,
-		width: 200,
-		margin: 10,
-	},
-	imagestyle: {
-		alignItems: 'flex-end',
-		height: 15,
-		width: 15,
-	},
-	profileView: {
-		alignItems: 'center',
-		backgroundColor: '#e6e8da',
-		height: '100%',
-	},
-	profileImage: {
-		height: 250,
-		width: 250,
-		marginBottom: 20,
-		marginTop: 20,
-		borderRadius: 125,
-		borderWidth: 5,
-		borderColor: '#e8984e',
-		alignItems: 'center',
-	},
-	profileName: {
-		color: '#1f142e',
-		fontStyle: 'italic',
-		fontWeight: '800',
-		fontSize: 20,
-		marginTop: 10,
-		marginBottom: 10,
-	},
+  button: {
+    backgroundColor: '#1F142E',
+    borderColor: '#bf90b1',
+    borderWidth: 5,
+    width: 200,
+    margin: 10,
+  },
+  imagestyle: {
+    alignItems: 'flex-end',
+    height: 15,
+    width: 15,
+  },
+  profileView: {
+    alignItems: 'center',
+    backgroundColor: '#e6e8da',
+    height: '100%',
+  },
+  profileImage: {
+    height: 250,
+    width: 250,
+    marginBottom: 20,
+    marginTop: 20,
+    borderRadius: 125,
+    borderWidth: 5,
+    borderColor: '#e8984e',
+    alignItems: 'center',
+  },
+  profileName: {
+    color: '#1f142e',
+    fontStyle: 'italic',
+    fontWeight: '800',
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
 });
 
 export default ProfileViewOther;
 
 //when creating a chat, you need to keep lines .update ---> merge so that it keeps two arrays to ultimately show you chats you are a participant in on nest view
 const createChat = () => {
-	db.collection('chats').doc(thisChatName).set(
-		{
-			chatName: thisChatName,
-		},
-		{ merge: true }
-	);
-	db.collection('chats')
-		.doc(thisChatName)
-		.update(
-			{
-				parties: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email),
-				photos: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.photoURL),
-			},
-			{ merge: true }
-		);
-	navigation.navigate('ChatScreen', { chatName: thisChatName });
+  db.collection('chats').doc(thisChatName).set(
+    {
+      chatName: thisChatName,
+    },
+    {merge: true}
+  );
+  db.collection('chats')
+    .doc(thisChatName)
+    .update(
+      {
+        parties: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email),
+        photos: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.photoURL),
+      },
+      {merge: true}
+    );
+  navigation.navigate('ChatScreen', {chatName: thisChatName});
 };
